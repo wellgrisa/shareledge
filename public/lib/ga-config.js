2269b56714542e242c344a64cc0784fa34a7b3c1
unknown <ramon.bozelli@gmail.com>
2014-09-16 01:28:04 -0300

    Setting up GA event traking from Login page and side menu bar on Home

---
+++ public/lib/ga-config.js
@@ -0,0 +1,28 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-54668389-3']);
+_gaq.push(['_trackPageview']);
+
+function ga_event(category, action, opt_label){      
+  _gaq.push(['_trackEvent', category, action, opt_label]);
+  console.log("*********** Tracking event: " + category + ", " + action + ", " + opt_label);
+}
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);      
+
+  // Mapping all elements which have GA Events
+  // data-gacategory="Login" data-gaaction="Register" data-galabel="Register new user"
+  $('*[data-gacategory]').each(function() {
+    var cat = $(this).data("gacategory");
+    var action = $(this).data("gaaction");
+    var label = $(this).data("galabel");
+
+    $(this).on("click", function() {
+      ga_event(cat, action, label);
+    });
+
+  });
+
+})();
