9c17aa5447953e3af40aae068d79bd100e47fcab
Ramon Bozelli <ramon.bozelli@gmail.com>
2014-11-18 21:34:34 -0200

    Google Analytics on Dashboard// Dashboard callbacks problem solved

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose'),
-  User = mongoose.model('User'),
-  Question = mongoose.model('Question');
+    User = mongoose.model('User'),
+    Question = mongoose.model('Question');
 
 exports.login = function(req, res) {
   res.render('login', { message: req.flash('loginMessage') });
@@ -143,54 +143,47 @@ exports.dashboard = function(req, res) {
   
   // Outstanding questions
   Question.find({solutions : {$size : 0}}).count().exec(function (err, data){
-    dash.outstandingQuestions = data;    
-		console.log('---------------// first');
-		console.log('\n---->', require('util').inspect(dash, { depth: 2, colors: true }));
-  });
+    dash.outstandingQuestions = data; 
 
-  // How many answered questions and marked as useful
-  Question.find({$and : [
-      {'solutions.0' : {$exists : true}},
-      {'useful' : {$gt : 0}}
-    ]}).count()
-  .exec(function (err, data){
-    dash.answeredQuestions = data;
-		console.log('---------------// second');
-		console.log('\n---->', require('util').inspect(dash, { depth: 2, colors: true }));
-  });
+    // How many answered questions and marked as useful
+    Question.find({$and : [
+        {'solutions.0' : {$exists : true}},
+        {'useful' : {$gt : 0}}
+      ]}).count()
+    .exec(function (err, data){
+      dash.answeredQuestions = data;
 
-  // How many people were helped (Just questions marked as useful)
-  Question.distinct(
-    'user',
-      {$and : [
-          {'solutions.0' : {$exists : true}},
-          {'useful' : {$gt : 0}}
-      ]}
-    )
-  .exec(function (err, data){
-    dash.helpedPeople = data.length;
-  });
+      // How many people were helped (Just questions marked as useful)
+      Question.distinct(
+        'user',
+          {$and : [
+              {'solutions.0' : {$exists : true}},
+              {'useful' : {$gt : 0}}
+          ]}
+        )
+      .exec(function (err, data){
+        dash.helpedPeople = data.length;
 
-  //Total of tags
-  Question.distinct('tags').exec(function (err, data){    
-    dash.totalTags = data.length;    
-  });
+        // Counting the most used tags
+        Question.aggregate([        
+            { $unwind : "$tags" },
+            { $project: { tags: { $toLower: "$tags" } } },
+            { $group: { _id: "$tags", count: { $sum: 1 } } },
+            { $sort: { count: -1 } },
+            { $limit : 6 } 
+          ])
+        .exec(function (err, data){        
+          dash.tags = data;  
 
-  // Counting the most used tags
-  Question.aggregate([        
-      { $unwind : "$tags" },
-      { $project: { tags: { $toLower: "$tags" } } },
-      { $group: { _id: "$tags", count: { $sum: 1 } } },
-      { $sort: { count: -1 } },
-      { $limit : 6 } 
-    ])
-  .exec(function (err, data){        
-    dash.tags = data;
+          //Total of tags
+          Question.distinct('tags').exec(function (err, data){    
+            dash.totalTags = data.length;
+
+            return res.render('admin/dashboard', { 'data' : dash });
+          });        
+        });
+      });
+    });   		
   });
-  
-	console.log('---------------//dash');
-	console.log('\n---->', require('util').inspect(dash, { depth: 2, colors: true }));
-	
-  return res.render('admin/dashboard', { 'data' : dash });
 };
 
